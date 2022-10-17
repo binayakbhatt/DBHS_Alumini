6dbcd93aca7ec12951b81b3a0e50cbb1a7a422af
Binayak <you@example.com>
2022-10-17 23:42:45 +0530

    update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,9 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => 'Admin',
-            'email' => 'admin@test.com',
+            'name' => 'Adminstrator',
+            'email' => 'adminstrator@test.com',
             'email_verified_at' => now(),
-            'role' => 1,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
