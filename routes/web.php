a0a111fd76ffbcf6d99eb53b4a5ef2109ab518c7
Binayak <you@example.com>
2022-10-30 19:42:59 +0530

    middleware admin removed

--- routes/web.php
+++ routes/web.php
@@ -27,14 +27,12 @@ Route::get('/news-detail/{slug}',[NewsController::class, 'news_detail'])->name('
 
 
 //dashboard
-Route::group(['middleware'=>['auth', 'can:admin']], function(){
-    Route::get('/dashboard',[DashboardController::class, 'display_count'] )->name('dashboard');
-
-    
+Route::group(['middleware'=>['auth','verfied']], function(){
+Route::get('/dashboard',[DashboardController::class, 'display_count'] )->name('dashboard');
 
 });
 //profile
-Route::group(['middleware'=>['auth']], function(){
+Route::group(['middleware'=>['auth','verfied']], function(){
     
     Route::view('profile', 'profile')->name('profile');
     Route::put('profile',[ProfileController::class,'update'])->name('profile.update');
