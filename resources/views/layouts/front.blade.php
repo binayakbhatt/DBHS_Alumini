37afd9f2836a764b82a4b2af8a59a88b35718288
Binayak <you@example.com>
2022-11-11 00:02:23 +0530

    footer added

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -29,8 +29,88 @@
         </main>
 
 
-        <footer class="bg-white shadow">
+        {{-- <footer class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex items-center justify-end">
+                Created by : 2005 Batch
+            </div>
+        </footer> --}}
+
+        <footer class="bg-cyan-100">
+            <div class="container mx-auto px-8">
+                <div class="w-full flex flex-col md:flex-row py-6">
+                    <div class="flex-1 mb-6 text-black">
+                        <a class="text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+                            href="/">
+
+                            <x-application-logo class="block h-15 w-auto fill-black" />
+
+                        </a>
+                    </div>
+                    <div class="flex-1">
+                        <p class="uppercase text-gray-500 md:mb-6">Links</p>
+                        <ul class="list-reset mb-6">
+                            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                                <a href="{{ route('front.news') }}"
+                                    class="no-underline hover:underline text-gray-800 hover:text-pink-500">News</a>
+                            </li>
+                            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                                <a href="{{ route('front.alumni') }}"
+                                    class="no-underline hover:underline text-gray-800 hover:text-pink-500">Alumni</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div class="flex-1">
+                        <p class="uppercase text-gray-500 md:mb-6">Legal</p>
+                        <ul class="list-reset mb-6">
+                            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                                <a href="#"
+                                    class="no-underline hover:underline text-gray-800 hover:text-pink-500">Terms</a>
+                            </li>
+                            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                                <a href="#"
+                                    class="no-underline hover:underline text-gray-800 hover:text-pink-500">Privacy</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="flex-1">
+                        <p class="uppercase text-gray-500 md:mb-6">Social</p>
+                        <ul class="list-reset mb-6">
+                            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                                <a href="#"
+                                    class="no-underline hover:underline text-gray-800 hover:text-pink-500">Facebook</a>
+                            </li>
+                            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                                <a href="#"
+                                    class="no-underline hover:underline text-gray-800 hover:text-pink-500">Linkedin</a>
+                            </li>
+                            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                                <a href="#"
+                                    class="no-underline hover:underline text-gray-800 hover:text-pink-500">Twitter</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="flex-1">
+                        <p class="uppercase text-gray-500 md:mb-6">Company</p>
+                        <ul class="list-reset mb-6">
+                            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                                <a href="#"
+                                    class="no-underline hover:underline text-gray-800 hover:text-pink-500">Official
+                                    Blog</a>
+                            </li>
+                            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                                <a href="#"
+                                    class="no-underline hover:underline text-gray-800 hover:text-pink-500">About Us</a>
+                            </li>
+                            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                                <a href="#"
+                                    class="no-underline hover:underline text-gray-800 hover:text-pink-500">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center justify-end">
                 Created by : 2005 Batch
             </div>
         </footer>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -30,6 +30,7 @@
                     stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
+
             </button>
             <ul class="
                   fixed
@@ -64,7 +65,8 @@
                     <form action="{{ route('logout') }}" method="POST">
                         @csrf
 
-                        <button class="bg-transparent hover:bg-red-900 text-white font-bold py-2 px-4 rounded-full border border-white border-solid border-2 ">
+                        <button
+                            class="bg-transparent hover:bg-red-900 text-white font-bold py-2 px-4 rounded-full border border-white border-solid border-2 ">
                             Logout</button>
                     </form>
                 @endif
