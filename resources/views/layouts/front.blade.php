80be4ada6ddb0ebc6a3ccaa0fc46c6379e6de142
Binayak <you@example.com>
2022-11-10 23:39:45 +0530

    update

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -27,6 +27,14 @@
         <main>
             {{ $slot }}
         </main>
+
+
+        <footer class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex items-center justify-end">
+                Created by : 2005 Batch
+            </div>
+        </footer>
+
     </div>
 
 </body>
